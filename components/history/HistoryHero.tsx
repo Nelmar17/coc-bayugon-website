8f5538498132516f667ca892c7e222d5ae5ca5ed
Nelmar17 <nelmar.buncag.it@gmail.com>
2026-02-17 15:47:55 +0800

    change h1 image to h1v2

--- components/history/HistoryHero.tsx
+++ components/history/HistoryHero.tsx
@@ -12,7 +12,7 @@ type Slide = {
 
 const SLIDES: Slide[] = [
   {
-    src: "/header-history/hd1.jpg",
+    src: "/header-history/h1-1v2.jpg",
     duration: 6000,
     overlayClass: "bg-gradient-to-b from-black/50 via-black/30 to-black/70",
   },
